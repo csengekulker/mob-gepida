fa1cccb047a2fe01e7d0727e3cbbc3804239576b
csengekulker <k71608883429@ktch.hu>
2023-04-25 21:16:30 +0200

    list loadable with data

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [bikes, setBikes] = useState([])
+
+  const fetchBikes = () => {
+    fetch(`http://localhost:3000/bikes`)
+    .then(res => res.json())
+    .then(data => setBikes(data))
+    .catch(err => console.error(err))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bikes</Text>
+
+      <Button 
+      title="Load"
+      onPress={fetchBikes}
+    />
+
+      <FlatList
+        style={styles.list}
+        data={bikes}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>{item.name} | </Text>
+            <Text>{item.wheel} | </Text>
+            <Text>{item.usage} | </Text>
+            <Text>{item.price}</Text>
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f8f9fa',
     alignItems: 'center',
     justifyContent: 'center',
+    width: 200
   },
+  item: {
+    flexDirection: 'row',
+  },
+  title: {
+    marginTop: 24,
+    fontSize: 34
+  },
+  list: {
+    padding: 15
+  }
 });
